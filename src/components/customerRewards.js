280601f0b423d3ec88be40137e9a2117419a2df1
bh15750 <suneela.patchava@macys.com>
2021-10-15 17:26:18 -0500

    styling changes

--- src/components/customerRewards.js
+++ src/components/customerRewards.js
@@ -60,44 +60,50 @@ export const CustomerRewards = () => {
 
   return (
     <div>
-      <h3>Monthly Customer Rewards</h3>
-      <div>
+      <h3 className='header'>Monthly Customer Rewards</h3>
+      <div className='container'>
         {Object.keys(monthlyCustomerPointTrans).map(month => {
           return (
             <>
-            <div style={{display:'flex'}}> 
               <div className="box">
-                <h3> Month: {month} </h3>
-                <hr/>
-                <table>
-                        <tr>
-                        <th>Customer Name</th>
-                        <th>Rewards</th>
-                        </tr>
-                {Object.keys(monthlyCustomerPointTrans[month]).map(
-                  customerName => (
+                <h3 style={{ padding: '5px' }}> Month: {month} </h3>
+                <hr />
+                <div>
+                  <table>
+                    <tr>
+                      <th>Customer Name</th>
+                      <th>Rewards</th>
+                    </tr>
+                    {Object.keys(monthlyCustomerPointTrans[month]).map(
+                      customerName => (
                         <tr>
                           <td>{customerName}</td>
                           <td>{monthlyCustomerPointTrans[month][customerName]}</td>
                         </tr>
-                      
-                  )
-                )}
-                </table>
-              </div>
+                      )
+                    )}
+                  </table>
+                </div>
               </div>
             </>
           );
         })}
       </div>
 
-      <h1>Total Customer Rewards</h1>
-      {Object.keys(totalCustomerPointTrans).map(customerName => (
-        <div>
-          <div className="label">{customerName}</div>
-          <div className="value">{totalCustomerPointTrans[customerName]}</div>
-        </div>
-      ))}
+      <h3 className='header'>Total Customer Rewards</h3>
+      <table style={{margin:'auto', border:'1px solid grey'}}>
+        <tr>
+          <th style={{width: '15vw'}}>Customer Name</th>
+          <th style={{width: '15vw'}}>Total Rewards</th>
+        </tr>
+        {Object.keys(totalCustomerPointTrans).map(customerName => (
+          <tr>
+            <td style={{width: '15vw'}}>{customerName}</td>
+            <td style={{width: '15vw'}}>{totalCustomerPointTrans[customerName]}</td>
+          </tr>
+
+        ))}
+      </table>
     </div>
   );
 };
